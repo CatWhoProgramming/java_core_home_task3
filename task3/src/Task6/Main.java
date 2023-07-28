d6fd3cff8ca901ecd33472333a60d4b05979d133
CatWhoProgramming <kat.servetnik@gmail.com>
2023-07-28 21:18:24 +0200

    In class Main we create objects of classes PrinterRed and PrinterBlue and print text in console with another colors

---
+++ task3/src/Task6/Main.java
@@ -0,0 +1,12 @@
+package Task6;
+
+public class Main {
+    public static void main(String[] args) {
+        PrinterRed printerRed = new PrinterRed();
+        printerRed.Print("I have some idea");
+        PrinterBlue printerBlue = new PrinterBlue();
+        printerBlue.Print("Just do it!");
+    }
+
+
+}
