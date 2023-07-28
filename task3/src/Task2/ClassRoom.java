4be60ed68e855a7b6e00bd397d9ef1b10b1ec51a
CatWhoProgramming <kat.servetnik@gmail.com>
2023-07-28 09:54:31 +0200

    Wrote Class Main in task2, in this class create object of class ClassRoom and get the value about pupils

--- task3/src/Task2/ClassRoom.java
+++ task3/src/Task2/ClassRoom.java
@@ -1,4 +1,25 @@
 package Task2;
 
 public class ClassRoom {
+    Pupil pupil1;
+    Pupil pupil2;
+    Pupil pupil3;
+    Pupil pupil4;
+
+    ClassRoom(Pupil pupil1, Pupil pupil2) {
+        this(pupil1, pupil2, pupil1);
+        this.pupil1 = pupil1;
+        this.pupil2 = pupil2;
+    }
+
+    ClassRoom(Pupil pupil1, Pupil pupil2, Pupil pupil3) {
+        this(pupil1);
+        this.pupil1 = pupil1;
+        this.pupil2 = pupil2;
+        this.pupil3 = pupil3;
+    }
+
+    ClassRoom(Pupil pupil4) {
+        this.pupil4 = pupil4;
+    }
 }
--- task3/src/Task2/Main.java
+++ task3/src/Task2/Main.java
@@ -1,4 +1,47 @@
 package Task2;
 
 public class Main {
+    public static void main(String[] args) {
+        ClassRoom classRoom = new ClassRoom(new GoodPupil());
+        classRoom = new ClassRoom(new BadPupil(),new ExcellentPupil(),new GoodPupil());
+         showAboutFirstPupilInformation(classRoom);
+         showAboutSecondPupilInformation(classRoom);
+         showAboutThirdPupilInformation(classRoom);
+         showAboutFourthPupilInformation(classRoom);
+
+    }
+
+    private static void showAboutFirstPupilInformation(ClassRoom classRoom) {
+        System.out.println("First pupil -----------------------------------");
+        classRoom.pupil1.study();
+        classRoom.pupil1.read();
+        classRoom.pupil1.relax();
+        classRoom.pupil1.write();
+        System.out.println("--------------------------------------------------");
+    }
+    private static void showAboutSecondPupilInformation(ClassRoom classRoom) {
+        System.out.println("Second pupil -----------------------------------");
+        classRoom.pupil2.study();
+        classRoom.pupil2.read();
+        classRoom.pupil2.relax();
+        classRoom.pupil2.write();
+        System.out.println("--------------------------------------------------");
+    }
+
+    private static void showAboutThirdPupilInformation(ClassRoom classRoom) {
+        System.out.println("Third pupil -----------------------------------");
+        classRoom.pupil3.study();
+        classRoom.pupil3.read();
+        classRoom.pupil3.relax();
+        classRoom.pupil3.write();
+        System.out.println("--------------------------------------------------");
+    }
+    private static void showAboutFourthPupilInformation(ClassRoom classRoom) {
+        System.out.println("Fourth pupil -----------------------------------");
+        classRoom.pupil4.study();
+        classRoom.pupil4.read();
+        classRoom.pupil4.relax();
+        classRoom.pupil4.write();
+        System.out.println("--------------------------------------------------");
+    }
 }
