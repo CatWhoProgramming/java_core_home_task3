af92e83898a2d44d285986e5e054b396ab759685
CatWhoProgramming <kat.servetnik@gmail.com>
2023-07-28 10:35:15 +0200

    Wrote class car

--- task3/src/Task3/Car.java
+++ task3/src/Task3/Car.java
@@ -1,4 +1,25 @@
 package Task3;
 
-public class Car extends Vehicle{
+import javax.swing.plaf.IconUIResource;
+
+
+public class Car extends Vehicle {
+    private double price;
+    private double speed;
+    private int year;
+
+    public Car(double price, double speed, int year) {
+        super(price, speed, year);
+        this.price = price;
+        this.speed = speed;
+        this.year = year;
+    }
+
+    void getParameters() {
+        System.out.println("--------------------------Car-------------------------");
+        System.out.println("Price: " + price);
+        System.out.println("Speed: " + speed);
+        System.out.println("Year: " + year);
+        System.out.println("------------------------------------------------------");
+    }
 }
