da8015f1f55f2525ad35d7aa400d9ef4a98a2d56
CatWhoProgramming <kat.servetnik@gmail.com>
2023-07-28 10:34:29 +0200

    Deleted method getParameters

--- task3/src/Task3/Vehicle.java
+++ task3/src/Task3/Vehicle.java
@@ -1,4 +1,13 @@
 package Task3;
 
 public class Vehicle {
+    private double price;
+    private double speed;
+    private int year;
+
+    public Vehicle(double price, double speed, int year) {
+        this.price = price;
+        this.speed = speed;
+        this.year = year;
+    }
 }
