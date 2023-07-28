48a050007cdb65c735c543f95b5d1485820f8f34
CatWhoProgramming <kat.servetnik@gmail.com>
2023-07-28 10:45:07 +0200

    Wrote base class DocumentWorker

---
+++ task3/src/Task4/DocumentWorker.java
@@ -0,0 +1,15 @@
+package Task4;
+
+public class DocumentWorker {
+    void openDocument() {
+        System.out.println("Document is open");
+    }
+
+    void editDocument() {
+        System.out.println("You can edit document in PRO version");
+    }
+
+    void saveDocument() {
+        System.out.println("You can save document in PRO version");
+    }
+}
