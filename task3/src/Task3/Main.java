59bfacea7b1ce0efd9e8a9ad75ef1fad26133e51
CatWhoProgramming <kat.servetnik@gmail.com>
2023-07-28 10:37:23 +0200

    In class main we created object from class Plane, Car,Ship and show parameters this vehicles

---
+++ task3/src/Task3/Main.java
@@ -0,0 +1,13 @@
+package Task3;
+
+public class Main {
+    public static void main(String[] args) {
+     Car car = new Car(40786, 250, 2005);
+     car.getParameters();
+     Plane plane = new Plane(200000, 1000,2019, 2000,56);
+     plane.getParameters();
+     Ship ship = new Ship(30000,300,2015, 45,"Odessa");
+     ship.getParameters();
+
+    }
+}
