cf7b74035e6361759f94c728e28abc8b9ef08e91
CatWhoProgramming <kat.servetnik@gmail.com>
2023-07-28 08:33:00 +0200

    Wrote class Pupil with 4 methods

--- task3/src/Task2/Pupil.java
+++ task3/src/Task2/Pupil.java
@@ -1,4 +1,19 @@
 package Task2;
 
 public class Pupil {
+    void study() {
+        System.out.println("I can study");
+    }
+
+    void read() {
+        System.out.println("I can read");
+    }
+
+    void write() {
+        System.out.println("I can write");
+    }
+
+    void relax() {
+        System.out.println("I can relax");
+    }
 }
