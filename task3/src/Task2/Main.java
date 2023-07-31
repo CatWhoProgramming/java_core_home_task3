9f545f4b8ef63af847e51f966a33d329d2e8464c
CatWhoProgramming <kat.servetnik@gmail.com>
2023-07-31 10:52:56 +0200

    Change class Main in Task2 Delete old methods that shows information about pupils, create one method that fill array in ClassRoom class

--- task3/src/Task2/Main.java
+++ task3/src/Task2/Main.java
@@ -2,46 +2,18 @@ package Task2;
 
 public class Main {
     public static void main(String[] args) {
-        ClassRoom classRoom = new ClassRoom(new GoodPupil());
-        classRoom = new ClassRoom(new BadPupil(),new ExcellentPupil(),new GoodPupil());
-         showAboutFirstPupilInformation(classRoom);
-         showAboutSecondPupilInformation(classRoom);
-         showAboutThirdPupilInformation(classRoom);
-         showAboutFourthPupilInformation(classRoom);
-
+        ClassRoom classRoom = new ClassRoom();
+        Pupil[] pupils = new Pupil[]{new GoodPupil(), new ExcellentPupil(), new BadPupil()};
+        fillClassRoom(classRoom,pupils);
+        classRoom.showInformationAboutPupils();
     }
 
-    private static void showAboutFirstPupilInformation(ClassRoom classRoom) {
-        System.out.println("First pupil -----------------------------------");
-        classRoom.pupil1.study();
-        classRoom.pupil1.read();
-        classRoom.pupil1.relax();
-        classRoom.pupil1.write();
-        System.out.println("--------------------------------------------------");
-    }
-    private static void showAboutSecondPupilInformation(ClassRoom classRoom) {
-        System.out.println("Second pupil -----------------------------------");
-        classRoom.pupil2.study();
-        classRoom.pupil2.read();
-        classRoom.pupil2.relax();
-        classRoom.pupil2.write();
-        System.out.println("--------------------------------------------------");
-    }
+    private static void fillClassRoom(ClassRoom classRoom,Pupil[] pupils) {
 
-    private static void showAboutThirdPupilInformation(ClassRoom classRoom) {
-        System.out.println("Third pupil -----------------------------------");
-        classRoom.pupil3.study();
-        classRoom.pupil3.read();
-        classRoom.pupil3.relax();
-        classRoom.pupil3.write();
-        System.out.println("--------------------------------------------------");
-    }
-    private static void showAboutFourthPupilInformation(ClassRoom classRoom) {
-        System.out.println("Fourth pupil -----------------------------------");
-        classRoom.pupil4.study();
-        classRoom.pupil4.read();
-        classRoom.pupil4.relax();
-        classRoom.pupil4.write();
-        System.out.println("--------------------------------------------------");
+        int randomIndex;
+        for (int i = 0; i < 4; i++) {
+            randomIndex = (int) (Math.random() * 3);
+            classRoom.pupils[i] = pupils[randomIndex];
+        }
     }
 }
