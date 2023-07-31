7bb49ce024c83d20409f933ed49be788ece6a3dc
CatWhoProgramming <kat.servetnik@gmail.com>
2023-07-31 10:50:27 +0200

    Change ClassRoom add constructor , method that show information about pupils, create array that iclude objects of class Pupil

--- task3/src/Task2/ClassRoom.java
+++ task3/src/Task2/ClassRoom.java
@@ -1,25 +1,20 @@
 package Task2;
 
 public class ClassRoom {
-    Pupil pupil1;
-    Pupil pupil2;
-    Pupil pupil3;
-    Pupil pupil4;
+    protected Pupil[] pupils;
 
-    ClassRoom(Pupil pupil1, Pupil pupil2) {
-        this(pupil1, pupil2, pupil1);
-        this.pupil1 = pupil1;
-        this.pupil2 = pupil2;
+    ClassRoom() {
+        this.pupils = new Pupil[4];
     }
 
-    ClassRoom(Pupil pupil1, Pupil pupil2, Pupil pupil3) {
-        this(pupil1);
-        this.pupil1 = pupil1;
-        this.pupil2 = pupil2;
-        this.pupil3 = pupil3;
+    public void showInformationAboutPupils() {
+        for (int i = 0; i < 4; i++) {
+            System.out.println("----------------------------------------");
+            this.pupils[i].study();
+            this.pupils[i].read();
+            this.pupils[i].write();
+            this.pupils[i].relax();
+        }
     }
 
-    ClassRoom(Pupil pupil4) {
-        this.pupil4 = pupil4;
-    }
 }
